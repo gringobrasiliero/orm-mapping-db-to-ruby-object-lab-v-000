3fc732d1095b0c7d6268c470f668a858d44874d7
Nolan Byrnes <nolanehs@gmail.com>
2018-01-03 13:23:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,9 +7,8 @@ new_student.id = row[0]
 new_student.name = row[1]
 new_student.grade = row[2]
 new_student
+end
 
-    # create a new Student object given a row from the database
-  end
 
   def self.all
 sql = <<-SQL
@@ -23,6 +22,19 @@ DB[:conn].execute(sql)
   end
 
   def self.find_by_name(name)
+sql = <<-SQL
+SELECT * 
+FROM students
+WHERE name = ?
+LIMIT 1
+SQL
+
+DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  
+
+
     # find the student in the database given a name
     # return a new instance of the Student class
   end
