2412f2b351da925150115dbfdb2b187650b058ad
Nolan Byrnes <nolanehs@gmail.com>
2018-01-03 14:13:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,12 @@ self.new_from_db(row)
 end
 
 def self.first_X_students_in_grade_10(x)
-
+sql = <<-SQL
+SELECT *
+FROM students
+WHERE grade = 10
+SQL
+DB[:conn].execute(sql)
 end
 
 def self.first_student_in_grade_10
