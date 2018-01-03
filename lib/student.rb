6a55c8e37ef34e2dcbdce4a6514e9146992d097e
Nolan Byrnes <nolanehs@gmail.com>
2018-01-03 14:06:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ SELECT name
 FROM students
 WHERE grade = 10
 SQL
-DB[:conn].execute(sql, name).map do |row|
+DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
 end
